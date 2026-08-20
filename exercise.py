f2e30f6eca3cc7dce94d7c085dee862b52a15e70
kaustuvacharya <kaustuvbricks@gmail.com>
2026-08-20 17:38:40 +0545

    password.txt

--- exercise.py
+++ exercise.py
@@ -2,5 +2,5 @@ number = float(int("enter the number:- "))
 if (number%2 == 0):
     print("the number is a even number") 
 else:
-    print("the number is a odd number") 
+    print("the number is a odd number")  
 
